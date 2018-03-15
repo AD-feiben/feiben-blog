e9042ac32f634dd2592e844e060434341cf7c6a3
shirunpei <shirunpei@chudaokeji.com>
2018-03-15 14:18:13 +0800

    调整文章样式

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -116,14 +116,36 @@ export default {
       font-size: 14px;
       li{display:list-item}
       head{display:none}
-      table{display:table}
-      tr{display:table-row}
-      thead{display:table-header-group}
+      table{
+        display:table;
+        width: 100%;
+        margin-bottom: 20px;
+        border: 1px solid #d9d9d9;
+        border-collapse: collapse;
+        border-left: none;
+        word-break: normal;
+      }
+      tr{
+        display:table-row;
+        &:nth-of-type(2n){
+          background-color: #f8f8f8;
+        }
+      }
+      thead{
+        display: table-header-group;
+        vertical-align: middle;
+        border-color: inherit;
+      }
       tbody{display:table-row-group}
       tfoot{display:table-footer-group}
       col{display:table-column}
       colgroup{display:table-column-group}
-      td,th{display:table-cell;}
+      td,th{
+        display:table-cell;
+        padding: 8px;
+        border: 1px solid #d9d9d9;
+        line-height: 20px;
+      }
       caption{display:table-caption}
       th{font-weight:bolder; text-align:center}
       caption{text-align:center}
@@ -132,6 +154,24 @@ export default {
       h2{font-size:1.5em; margin:.75em 0}
       h3{font-size:1.17em; margin:.83em 0}
       h4,p,blockquote,ul,fieldset,form,ol,dl,dir,menu{margin:1.12em 0}
+      blockquote{
+        color: #666;
+        border-left: 4px solid #ddd;
+        padding-left: 20px;
+        margin-left: 0;
+        font-size: .875em;
+        p{
+          margin: 0;
+          font-style: italic;
+        }
+      }
+      img{
+        max-width: 100%;
+        width: auto;
+        height: auto;
+        vertical-align: middle;
+        border: 0;
+      }
       h5{font-size:.83em; margin:1.5em 0}
       h6{font-size:.75em; margin:1.67em 0}
       h1,h2,h3,h4,h5,h6,b,strong{font-weight:bolder}
@@ -142,7 +182,7 @@ export default {
         overflow-x: scroll;
       }
       // pre,tt,code,kbd,samp{font-family:monospace}
-      pre{white-space:pre}
+      pre{white-space:pre; padding: 5px;}
       button,textarea,input,object,select{display:inline-block;}
       big{font-size:1.17em}
       small,sub,sup{font-size:.83em}
@@ -152,7 +192,11 @@ export default {
       thead,tbody,tfoot{vertical-align:middle}
       td,th{vertical-align:inherit}
       s,strike,del{text-decoration:line-through}
-      hr{border:1px inset}
+      hr{
+        margin: 0 0 20px;
+        border: 0;
+        border-top: 1px solid #d9d9d9!important;
+      }
       ol,ul,dir,menu,dd{margin-left:40px}
       ol{
         li{list-style-type:decimal}
